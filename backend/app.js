f3faa0b6e2bdb2a576557f50791818d338971f0b
avkhalkar <96734791+avkhalkar@users.noreply.github.com>
2026-04-14 05:44:46 +0530

    Added temporary dummy getUserThreads route (to be replaced by person A with the actual route)

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,10 @@ import passport from "./config/passport.js";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import threadRoutes from "./routes/threadRoutes.js";
-//import messageRoutes from "./routes/messageRoutes.js";
 import forumRoutes from "./routes/forumRoutes.js";
 import subForumRoutes from "./routes/subForumRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js"; // 🔥 IMPORTANT
+import dummyAuthRoutes from "./routes/dummyAuthRoutes.js"; // 🧪 TEMPORARY: Remove after Person A wires getUserThreads
 
 import globalErrorHandler from "./controllers/errorController.js";
 import { AppError } from "./utils/appError.js";
@@ -52,7 +52,8 @@ app.use("/api/threads", threadRoutes);
 //app.use("/api/messages", messageRoutes);
 app.use("/api", forumRoutes);
 app.use("/api", subForumRoutes);
-app.use("/api/comments", commentRoutes); 
+app.use("/api/comments", commentRoutes);
+app.use("/api/dummy", dummyAuthRoutes); // 🧪 TEMPORARY: Test getUserThreads
 
 // 404 handler
 app.use((req, res, next) => {
