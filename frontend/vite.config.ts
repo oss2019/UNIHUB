3455bcb5608105374c8941564e1c6642eb3c18e2
latakshsariyapatidar <latakshpatidar@gmail.com>
2026-05-03 02:56:45 +0530

    changed the base in the configs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+    base: '/peerhive/',
   plugins: [react(), tailwindcss(), tsconfigPaths()],
   server: {
     port: 6442,
